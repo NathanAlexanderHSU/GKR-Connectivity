77845a84e1dae03d976a4c698bdc7e2de79e31db
timbean <timbean@gmail.com>
2016-03-29 09:13:24 -0700

    Created R Project and created "main.r" for centralizing all the code

---
+++ main.R
@@ -0,0 +1,7 @@
+# This is a script that (theoretically) will run EVERYTHING
+
+# 1. Reclassify predictor rasters
+source("R Code Reclassification")
+
+# 2. Create cost surfaces
+source("R Code Cost Surface Creation")
