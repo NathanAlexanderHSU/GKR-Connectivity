5b321caec50bd7ad29915a15f48082fc5b627917
NathanAlexanderHSU <nba52@humboldt.edu>
2016-05-10 11:38:34 -0700

    Update and rename R Code Moran's Eigenvector Map to R Code MEM.R

--- R Code MEM.R
+++ R Code MEM.R
@@ -1,10 +1,11 @@
 ## Memgene analysis
+##Can plot on Google maps if using Decimal Lat/Lon, but not UTMS
 
 install.packages("memgene")
 library(memgene)
 library(adegenet)
 ## Read in genotypes and coordinates
-genalex <- read.csv(choose.files(), header=TRUE)
+genalex <- read.csv(choose.files(), header=TRUE)  ##Read in raw loci values, not genetic distances
 genalex<-genalex[-1,]
 head(genalex)
 dim(genalex)
@@ -12,11 +13,17 @@ dim(genalex)
 Genotypes <- genalex[1:121,3:32]
 Coords <- genalex[1:121,34:35]
 (Genotypes)
-## Produce genetic distance matrix using proportion of shared alleles
-dm <- codomToPropShared(Genotypes)
+
+## Produce genetic distance matrix using proportion of shared alleles (Bowcock et al. 1994)
+
+dm <- codomToPropShared(Genotypes)  
+
 ## Extract Moran's Eigen Vectors from distance matrix
+
 if(!exists("Analysis"))
 Analysis <- mgQuick(dm,Coords)
+
+
 ## Visualize results of the first two memgene variables 
 mgMap(Coords, Analysis$memgene[,1])
 
