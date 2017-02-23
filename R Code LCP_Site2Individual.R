22cb70d7bf0a48ffd56a89b61c057ed41711d8b6
NathanAlexanderHSU <nba52@humboldt.edu>
2017-02-23 13:50:26 -0600

    Update R Code LCP_Site2Individual.R
    updated for 2015 data analysis

--- R Code LCP_Site2Individual.R
+++ R Code LCP_Site2Individual.R
@@ -7,11 +7,12 @@
 
 ##read in a file with the krat individuals and the krat sites
 
-KratSite<-read.csv("H:\\THESISDATA\\KRAT_SITE_FILES\\Krat_Lat.csv")
+KratSite<-read.table("C:\\Users\\Nathan\\Desktop\\2015_CS\\2015_occurrences_SITE.txt")
 
 ##read in a file that has sites associated with circuitscape numeric values (order of nodes)
 
-SiteNum<-read.csv("H:\\THESISDATA\\KRAT_SITE_FILES\\Lat_CS.csv")
+SiteNum<-read.table("C:\\Users\\Nathan\\Desktop\\2015_CS\\2015_occurrences.txt")
+
 
 head(SiteNum)
 length(SiteNum$Cscode)
@@ -20,13 +21,13 @@ head(KratSite)
 
 ##merge the two datasets to allow each individual to have a population based Circuitscape code
 
-KratNum<-merge(KratSite,SiteNum,by="Lat")
+KratNum<-merge(KratSite,SiteNum,by=c("V2","V3"))
 head(KratSite)
 head(KratNum)
 
 ##rename the column names
 
-names(KratNum)<-c("Lat","ID","CScode")
+names(KratNum)<-c("Lon","Lat","CScode","IndID")
 
 ##Merging made the individuals get out of order. To use individuals, we need to re-order
 ##based on the genetic distance order
@@ -40,38 +41,39 @@ head(OrderedData)
 
 
 
-x<-(OrderedData$CScode)
+x<-(KratNum$CScode)
 length(x)
 ##read in cost maps
 setwd(choose.dir())
-LCPmaps<-list.files("H:\\THESISDATA\\LCPDist\\")
-LCPmaps<-LCPmaps[-1]
 LCPmaps<-list.files()
-LCPmaps
+#LCPmaps<-LCPmaps[-1]
+#LCPmaps<-list.files()
+#LCPmaps
 ##read in file with the Circuitscape ID assignment
 
 SiteNum<-read.table("H:\\THESISDATA\\GKR_LatLon\\GKR_Circuitscape_Site.txt")
 colnames(SiteNum)<-c("CSCode","X","Y")
 
 for(k in 1:length(LCPmaps)){
+model<-read.table(LCPmaps[1],header=F)
   model<-read.table(LCPmaps[k],header=F)
- 	M<-matrix(NA,ncol=47,nrow=47)
+ 	M<-matrix(NA,ncol=72,nrow=72)
 	M[lower.tri(M,diag=F)]<-model[,3]
 	M[upper.tri(M,diag=F)]<-t(M)[upper.tri(M)]
 	M[is.na(M)]<-0
 
-  row.names(M)<-SiteNum$CSCode
-  colnames(M)<-SiteNum$CSCode
-  
-  Individual<-matrix(NA,ncol=121,nrow=121) ##Create a null matrix of length of individuals
-  for(i in 1:121){
+  row.names(M)<-KratNum$CSCode
+  colnames(M)<-KratNum$CSCode
+
+  Individual<-matrix(NA,ncol=239,nrow=239) ##Create a null matrix of length of individuals
+  for(i in 1:239){
     
-    for(j in 1:121){
+    for(j in 1:239){
       
-      Individual[i,j]<-M[paste(c(x[i]),collapse="",sep=","),paste(c(x[j]),collapse="",sep=",")]
+      Individual[i,j]<-M[x[i],x[j]]
     }
   }
   Data<-data.frame(Individual)
- write.csv(Individual,paste(c("H:\\THESISDATA\\IndLCPDist\\NEED",LCPmaps[k],".csv"),sep="",collapse=""))
+ write.csv(Individual,paste(c("C:\\Users\\Nathan\\Desktop\\2015THESISDATA\\2015_LCP\\LCP_Param_Ind\\",LCPmaps[k],".csv"),sep="",collapse=""))
   
 }
