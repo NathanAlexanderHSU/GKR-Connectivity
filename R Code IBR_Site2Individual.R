d12230b95bb29d050835b5600d60e8c52b74d036
NathanAlexanderHSU <nba52@humboldt.edu>
2017-06-22 18:44:16 -0500

    Update R Code IBR_Site2Individual.R
    updated code for 2015 data inclusion

--- R Code IBR_Site2Individual.R
+++ R Code IBR_Site2Individual.R
@@ -18,7 +18,7 @@ head(KratSite)
 head(KratNum)
 
 ##rename the column names
-
+KratNum
 names(KratNum)<-c("Lat","ID","CScode")
 
 ##Merging made the individuals get out of order. To use individuals, we need to re-order
@@ -39,29 +39,32 @@ length(x)
 #########read in cost maps
 
 IBRmaps<-list.files("H:\\THESISDATA\\IBRDistMatrices\\")
-IBRmaps
+IBRmaps<-choose.files()
 
 #######read in file with the Circuitscape ID assignment
 
 SiteNum<-read.table("H:\\THESISDATA\\GKR_LatLon\\GKR_Circuitscape_Site.txt")
-
+IBRmaps<-choose.files()
+IBRmaps
 for(k in 1:length(IBRmaps)){
   model<-read.csv(paste(c("H:\\THESISDATA\\IBRDistMatrices\\",IBRmaps[k]),sep="",collapse=""),header=T)
+  model<-read.table(choose.files(),header=TRUE)
   model<-model[,-1] ##Get rid of list of names
-  row.names(model)<-SiteNum[,1]
-  colnames(model)<-SiteNum[,1]
+KratNum
+rownames(model)
+  row.names(model)<-unique(KratNum[,3])
+  colnames(model)<-unique(KratNum[,3])
+  x<-(KratNum$CScode)
+
+  Individual<-matrix(NA,ncol=239,nrow=239) ##Create a null matrix of length of individuals
+  for(i in 1:239){
   
-  Individual<-matrix(NA,ncol=121,nrow=121) ##Create a null matrix of length of individuals
-  for(i in 1:121){
-    
-    for(j in 1:121){
+    for(j in 1:239){
       
       Individual[i,j]<-model[paste(c(x[i]),collapse="",sep=","),paste(c(x[j]),collapse="",sep=",")]
     }
   }
   Data<-data.frame(Individual)
-  write.csv(Individual,paste(c("H:\\THESISDATA\\IndIBRDist\\",IBRmaps[k]),sep="",collapse=""))
+  write.csv(Individual,paste(c("C:\\Users\\Nathan\\Desktop\\2015THESISDATA\\MaxEnt_IBR_Conductance_Dps.csv"),sep="",collapse=""))
   
 }
-
-
