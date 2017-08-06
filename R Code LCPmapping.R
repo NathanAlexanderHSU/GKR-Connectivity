a8b5e5597edcd863979ea69ff133df9189dc46b4
NathanAlexanderHSU <nba52@humboldt.edu>
2017-08-05 21:35:19 -0500

    Update R Code LCPmapping.R
    updated to create shapefiles from the LCP maps

--- R Code LCPmapping.R
+++ R Code LCPmapping.R
@@ -4,19 +4,26 @@ install.packages("gdistance")
 install.packages("rgdal")
 install.packages("raster")
 require(gdistance)
+require(maptools)
 require(raster)
 require(rgdal)
 require(sp)
-gkr.pts <- readOGR("F:\\GKRThesisCoding",layer="ocpt")
-patch.locs <- as.data.frame(gkr.pts)
-names(patch.locs)<-c("pointID","gridID","x","y")
+require(ggplot2)
+#gkr.pts <- readOGR("F:\\GKRThesisCoding",layer="ocpt")
+
+patch.locs <- as.data.frame(read.table(choose.files()))
+names(patch.locs)<-c("pointID","x","y")
 head(patch.locs)
 gkr.pts$ident <- as.integer(patch.locs$pointID)
-maps <- list.files(path="H:\\R_CS\\CM")    #prompt user for dir containing raster files
-e <- extent(685000,730000,4030000,4065000)
+maps <- list.files(choose.dir(),recursive=FALSE)    #prompt user for dir containing raster files
+maps
+e <- extent(raster(maps[1]))
+setwd(choose.dir())
 plot(e)
-plot(raster(paste("H:\\R_CS\\CM\\model_2costmap_asc.asc")),legend=FALSE,xlim=c(684900,730000),ylim=c(4030000,4065000),add=TRUE)
-head(gkr.pts)
+plot(raster(paste(maps[1])),legend=FALSE,xlim=c(684900,730000),ylim=c(4030000,4065000),add=TRUE)
+LCP <- SpatialLines()
+
+points(patch.locs[,2:3],pch=25,fill="white")
 plot(gkr.pts[1,],add=TRUE,pch=17)
 plot(gkr.pts[6,],add=TRUE,pch=17)
 plot(gkr.pts[104,],add=TRUE,pch=17)
@@ -24,18 +31,39 @@ plot(gkr.pts[104,],add=TRUE,pch=17)
 
 for(k in 1:length(maps)){
   
-  cost.surface <- raster(paste("H:\\R_CS\\CM\\",maps[k],collapse='',sep=""))
+  cost.surface <- raster(paste(maps[k]))
   tr <- transition(1/cost.surface, transitionFunction=sum, directions=8)
   tr.corr <- geoCorrection(tr, type="c")
   
-  
-  
   from.patch <- patch.locs[1,]
   
-  to.patch <- patch.locs[6,]
+  to.patch <- patch.locs[2,]
   from.loc <- c(from.patch$x, from.patch$y)
   to.loc <- c(to.patch$x, to.patch$y)
   cost <- shortestPath(tr.corr, from.loc, to.loc,output="SpatialLines")
-  lines(cost,col="red")
+  LCP <- cost
+
+  
+  for(i in 1:(length(patch.locs[,1])-1)){
+    
+    for(z in 3:(length(patch.locs[,1]))) {
+  from.patch <- patch.locs[i,]
+  
+  to.patch <- patch.locs[z,]
+  from.loc <- c(from.patch$x, from.patch$y)
+  to.loc <- c(to.patch$x, to.patch$y)
+  cost <- shortestPath(tr.corr, from.loc, to.loc,output="SpatialLines")
+  LCP <- rbind(LCP, cost)
+  
+    }
+  }
+
+  df<-data.frame(1:length(LCP))
+  row.names(df)<-row.names(LCP)
+
+  LCP_SLDF<-SpatialLinesDataFrame(LCP,data=df)
+  
+    writeOGR(LCP_SLDF, dsn="C:/Users//Nathan/Desktop/LCP_shapefiles" ,layer=paste0("LCP_",maps[k]),driver="ESRI Shapefile")
 }
+
 
