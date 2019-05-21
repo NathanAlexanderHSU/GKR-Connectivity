919eefd855a31ccb15b960b83e414a708d74722e
NathanAlexanderHSU <nba52@humboldt.edu>
2019-05-21 13:43:19 -0500

    Update 3_R code LCP 2Site.R
    notated

--- 3_R code LCP 2Site.R
+++ 3_R code LCP 2Site.R
@@ -2,20 +2,28 @@ require(gdistance)
 require(raster)
 require(rgdal)
 require(sp)
+
+#read in the patch files. Each of these is a unique location
 patch.locs<-read.table(choose.files())
 patch.locs <- read.table("H:\\THESISDATA\\GKR_LatLon\\GKR_Circuitscape_Sites.txt")  #Text file of locs
+
+#specify column names. Format is a text file with site ID, Longitude, Latitude
 names(patch.locs)<-c("ID","x","y")
 head(patch.locs)
+
+#list all the raster files that will have costs calculated across them
 maps <- list.files(path="I:\\THESISDATA\\Parameterization\\ascMaps\\")    #prompt user for dir containing raster files
 maps
-for(k in 5:length(maps)){
+
+#cycle through calculating for each raster
+for(k in 1:length(maps)){
 
 	cost.surface <- raster(paste("I:\\THESISDATA\\Parameterization\\ascMaps\\",maps[k],collapse='',sep=""))
 	tr <- transition(1/cost.surface, transitionFunction=sum, directions=8)
 	tr.corr <- geoCorrection(tr, type="c")
 	patch.dist <- NULL
 
-
+#cycle trhough each site
 for(i in 1:(nrow(patch.locs)-1)){
 	from.patch <- patch.locs[i,]
 
